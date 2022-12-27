ba001bb90454569944d2480342d92e50d8ad7aa8
Igor Vorona <voronkpi91@gmail.net>
2022-12-27 11:10:55 +0200

    add EncodingFilter

---
+++ src/main/java/com/epam/web/filter/EncodingFilter.java
@@ -0,0 +1,53 @@
+package com.epam.web.filter;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+
+    private static final Logger log = LogManager.getLogger(EncodingFilter.class);
+
+    private String encoding;
+
+    public void init(FilterConfig fConfig) throws ServletException {
+        log.debug("Filter initialization starts");
+
+
+        encoding = fConfig.getInitParameter("encoding");
+        log.trace("encoding from config ==> " + encoding);
+
+        log.debug("Filter initialization finished");
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        log.debug("EncodingFilter starts");
+        request.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        log.trace("Request uri --> " + httpRequest.getRequestURI());
+
+        String requestEncoding = request.getCharacterEncoding();
+        if (requestEncoding == null) {
+            log.trace("Request encoding = null, set encoding --> " + encoding);
+            request.setCharacterEncoding(encoding);
+        }
+
+        log.debug("Filter finished");
+        chain.doFilter(request, response);
+    }
+
+    public void destroy() {
+        log.debug("Filter encoding destroy");
+    }
+}
